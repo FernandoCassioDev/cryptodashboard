ee442891e58ab709a1ac0a4bc59705ed55984c12
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-31 20:22:43 -0300

    chore: importa fontes e configura html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        min: {
+          bg: "#151515",
+        },
+      },
+    },
+    fontFamily: {
+      grotesk: ["Space Grotesk", "sans-serif"],
+      noto: ["Noto Sans", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
