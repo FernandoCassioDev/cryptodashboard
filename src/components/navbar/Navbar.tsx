bfebebc38ee1c11f6d7729b2488fbf2d66b4a732
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-31 20:45:38 -0300

    style: ajusta estilização da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 function Navbar() {
   return (
     <div className="bg-min-bg h-16 font-grotesk flex justify-center items-center text-indigo-100 border-b">
-      <div className="container justify-between flex flex-row">
-        <div className="flex gap-2 hover:text-white">
+      <div className="container justify-between flex flex-row ">
+        <div className="flex gap-5 hover:text-white">
           <img src="./src/assets/bcio.svg"  alt="Icone Blockchain" />
           <h1 className="text-2xl">Crypto Insights</h1>
         </div>
-        <div className="flex gap-10 ">
+        <div className="flex gap-10">
           <a href="" className="hover:text-white">Dashboard</a>
           <a href="" className="hover:text-white"> Markets</a>
           <a href="" className="hover:text-white">Assets</a>
