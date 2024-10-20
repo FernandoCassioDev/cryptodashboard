878dbffa34e7e7b6606c72c572833d7d6bf354ae
Fernando <fernandocassiodev@gmail.com>
2024-10-20 15:19:45 -0300

    feat: adiciona responsividade no DataCoin para telas menores, falta arrumar a formatação dos preços.

--- src/components/coins/coin/DataCoin.tsx
+++ src/components/coins/coin/DataCoin.tsx
@@ -5,14 +5,21 @@ interface CoinProps {
 }
 
 function DataCoin({ coin }: CoinProps) {
+  const coinPriceInUSD = new Intl.NumberFormat().format(
+    coin.price_usd
+  );
+
   return (
     <>
-      <div className="flex flex-row justify-between items-center min-w-full border-b border-gray-500 min-h-14 font-semibold px-8">
-        <span className="w-32 flex flex-row justify-center">{coin.name}</span>
-        <span className="w-32 flex flex-row justify-center">
-          {coin.price_usd.toFixed(3)}
+      <div className="flex min-w-full justify-between items-center border-b border-gray-500 min-h-14 font-semibold px-4 md:px-8">
+        <span className="w-20 text-sm md:w-32 flex flex-row justify-center">
+          {coin.name}
         </span>
-        <span className="w-32 flex flex-row justify-center">
+        {/* arrumar formatação do preço para telas menores */}
+        <span className="w-20 text-sm md:w-32 flex flex-row justify-center">
+          {coinPriceInUSD}
+        </span>
+        <span className="hidden text-sm w-20 md:flex flex-row justify-center md:w-32">
           {coin.volume_1day_usd}
         </span>
       </div>
